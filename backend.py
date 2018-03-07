4353542e5bc9a206d9d20bad140afb830cf73b6c
Shawn Hatchwell <shawn.hatchwell@ufl.edu>
2018-03-07 00:43:45 -0500

    Starting to add Yelp API

--- backend.py
+++ backend.py
@@ -15,6 +15,10 @@ tempF = 1.8 * (float(tempK) - 273) + 32
 print "The temperature at that location is "+str(tempF)+" degrees Fahrenheit."
 
 if(tempF>=73.4):
-    print "It is HOT (>=74.3)"
+    print "It is HOT (>=73.4)"
+    host = 'https://api.yelp.com'
+    search = '/v3/businesses/search'
+    key = 'EVfllif2av6cSnIipleMTTNh4UKudxAyVr69DL3aQbW_5BREl1TinQFKligE9OdhAgxq82ddYrPPNdp-Yx0N_r3WiRorrcFYBID2NZ9XTrxGyfoe0taa5Xhmvm-fWnYx'
 else:
-    print "It is COLD (<74.3)"
+    print "It is COLD (<73.4)"
+    
